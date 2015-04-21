9558cf763db39aca9522fa016fb0e2403b44cf59
leslie <leslie89757@126.com>
2015-04-21 09:41:52 +0800

    Initial Commit

---
+++ PhotoRateDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoRateDemo
+//
+//  Created by leslie on 15/4/21.
+//  Copyright (c) 2015年 leslie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoRateDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoRateDemo
+//
+//  Created by leslie on 15/4/21.
+//  Copyright (c) 2015年 leslie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
